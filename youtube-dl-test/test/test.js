184200a4a7e220135908acb430943f1e6d30dbd8
allansm <allan.de.souza.melo.rj@gmail.com>
2021-09-02 18:45:10 -0300

    ...

--- youtube-dl-test/test/test.js
+++ youtube-dl-test/test/test.js
@@ -1,55 +1,70 @@
 const youtubedl = require('youtube-dl-exec')
 
-let flag = true;
+function getInfo(video){
+	return youtubedl(video, {
+		dumpSingleJson: true,
+		noWarnings: true,
+		noCallHome: true,
+		noCheckCertificate: true,
+		preferFreeFormats: true,
+		youtubeSkipDashManifest: true,
+		referer: video
+	});	
+}
+
+//let flag = true;
 function test(output){
 	/*output["formats"].forEach(function (format){
 		console.log("url:"+format["url"]+"\next:"+format["ext"]+"\n");
 	});*/
 	//console.log(output["title"]);
 	//console.log(output);
-	
+	console.log(output);	
 }
-function getPlayList(output){
+function getPlaylist(output,lambda){
+	var playlist = "";
 	var i = 0;
-	var arr = "";
+
 	output["entries"].forEach(function (out){
-		arr[i++] = out["webpage_url"];
+		playlist[i++] = out["webpage_url"];
 	});
 
-	return arr;
+	lambda(playlist);	
 }
 
-function test2(output){
+function getTitle(output,lambda){
+	lambda(output["title"]);	
+}
+
+function getFormats(output,lambda){
+	lambda(output["formats"]);
+}
+
+function getUrl(formats){
+	formats.forEach(function (format){
+		console.log("url:"+format["url"]+"\next:"+format["ext"]+"\n");
+	});
+}
+
+/*function test2(output){
 	var tmp = output;
 	console.log(tmp);
-}
+}*/
 var playlist = "";
-var video = "https://www.youtube.com/playlist?list=PLIY8eNdw5tW_uaJgi-FL9QwINS9JxKKg2";//"https://www.youtube.com/watch?v=uBBDMqZKagY";
-/*
-youtubedl(video, {
-  dumpSingleJson: true,
-  noWarnings: true,
-  noCallHome: true,
-  noCheckCertificate: true,
-  preferFreeFormats: true,
-  youtubeSkipDashManifest: true,
-  referer: video
-})
-  .then(output => test2(output))
-*/
+//playlist : "https://www.youtube.com/playlist?list=PLIY8eNdw5tW_uaJgi-FL9QwINS9JxKKg2";
+//video : https://www.youtube.com/watch?v=uBBDMqZKagY";
 
-const test3 = async () =>{
-	const v = youtubedl(video, {
-	  dumpSingleJson: true,
-	  noWarnings: true,
-	  noCallHome: true,
-	  noCheckCertificate: true,
-	  preferFreeFormats: true,
-	  youtubeSkipDashManifest: true,
-	  referer: video
+
+var info = getInfo("https://www.youtube.com/playlist?list=PLIY8eNdw5tW_uaJgi-FL9QwINS9JxKKg2");
+
+var show = function (playlist){
+	playlist.forEach(function (link){
+		console.log(link)
 	});
-	return await v;
+};
 
+var tst = function (entries){
+	console.log(entries);
 }
 
-console.log(test3);
+info.then(output => getPlaylist(output,show));
