de4957d3115d54da2d50d4dce6c805254755bc76
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-19 16:44:14 -0300

    ...

--- test/random.js
+++ test/random.js
@@ -3,11 +3,33 @@ function rand(min,max){
 }
 
 function test(){
-	var arr = [1,2,3];
-
-	console.log(arr);
+	var arr = [1,2,3,4,5];
+	var index = [];
+	
+	var i=0;
+	while(index.length < arr.length){
+		var tmp = rand(0,arr.length-1);
+		var flag = true;
+		for(var n=0;n<index.length;n++){
+			if(index[n] == tmp){
+				flag = false;
+			}
+		}
+		if(flag){
+			index[i++] = tmp;
+		}
+	}
+	
+	var ret = [];
+	i=0;
+	for(var n=0;n<index.length;n++){
+		ret[i++] = arr[index[n]];	
+	}
+	console.clear();
+	console.log(ret);	
 }
 
-console.log(rand(0,10))
+console.log(rand(0,10));
 
-test()
+setInterval(test,100);
+
